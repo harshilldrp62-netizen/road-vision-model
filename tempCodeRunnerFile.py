e94f1c762c8f00bd9ac7a2a241d46e25478a5572
Harshil Prajapati <harshilldrp62@gmail.com>
2026-08-15 13:00:52 +0530

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-results = model(path, conf=0.1)
-    # results[0].save(filename=f"demo_output/real-world-img/result-{img}") # ADD PATH FOR THE RESULT BOX
-
-    # heatmap = generate_heatmap(path, model)
-    # cv2.imwrite(f"demo_output/real-world-img/heatmap_{img}", heatmap) # ADD PATH FOR THE HEATMAP
-    # print(f"Done: {img}")
